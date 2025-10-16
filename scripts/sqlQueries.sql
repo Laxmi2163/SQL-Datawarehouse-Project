7f3b56bb25f25e3bb9d8e75da5826c172524fee7
Laxmi2163 <nagulalaxmiprasanna12@gmail.com>
2025-10-16 14:40:59 +0530

    sqlQueries.sql

--- scripts/sqlQueries.sql
+++ scripts/sqlQueries.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE datawarehouse4;
-
+CREATE DATABASE datawarehouse;
+use datawarehouse;
 
 CREATE Schema bronze;
 
@@ -20,13 +20,12 @@ cst_create_date date
 );
 
 BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\Anudeep\Downloads\Sql_files\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+FROM 'C:\Users\Laxmi Prasanna\OneDrive\Desktop\datasets\datasets\source_crm\cust_info.csv'
 WITH (
  FIRSTROW = 2,
  FIELDTERMINATOR = ',',
  TABLOCK
 );
-
 
 
 
@@ -41,7 +40,7 @@ CREATE TABLE bronze.crm_prd_info (
 );
 
 BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\Anudeep\Downloads\Sql_files\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+FROM 'C:\Users\Laxmi Prasanna\OneDrive\Desktop\datasets\datasets\source_crm\prd_info.csv'
 WITH (
  FIRSTROW = 2,
  FIELDTERMINATOR = ',',
@@ -61,7 +60,7 @@ CREATE TABLE bronze.crm_sales_details (
 );
 
 BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\Anudeep\Downloads\Sql_files\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+FROM 'C:\Users\Laxmi Prasanna\OneDrive\Desktop\datasets\datasets\source_crm\sales_details.csv'
 WITH (
  FIRSTROW = 2,
  FIELDTERMINATOR = ',',
@@ -80,7 +79,7 @@ CREATE TABLE bronze.erp_cust_az12 (
 
 
 BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\Anudeep\Downloads\Sql_files\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+FROM 'C:\Users\Laxmi Prasanna\OneDrive\Desktop\datasets\datasets\source_erp\CUST_AZ12.csv'
 WITH (
  FIRSTROW = 2,
  FIELDTERMINATOR = ',',
@@ -95,7 +94,7 @@ CREATE TABLE bronze.erp_loc_a101 (
 
 
 BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\Anudeep\Downloads\Sql_files\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+FROM 'C:\Users\Laxmi Prasanna\OneDrive\Desktop\datasets\datasets\source_erp\LOC_A101.csv'
 WITH (
  FIRSTROW = 2,
  FIELDTERMINATOR = ',',
@@ -112,7 +111,7 @@ CREATE TABLE bronze.erp_px_cat_g1v2 (
 
 
 BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\Anudeep\Downloads\Sql_files\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+FROM 'C:\Users\Laxmi Prasanna\OneDrive\Desktop\datasets\datasets\source_erp\PX_CAT_G1V2.csv'
 WITH (
  FIRSTROW = 2,
  FIELDTERMINATOR = ',',
